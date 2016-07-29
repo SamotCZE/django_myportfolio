24a67df1f74176a8764dd70b2e0a525f2c7a4e88
Tomas Hudecek <thomas.hudecek@gmail.com>
2016-07-30 01:03:48 +0200

    responsive intro page for small laptops

--- mysite/static/js/scripts.js
+++ mysite/static/js/scripts.js
@@ -14,8 +14,8 @@ function fullPageInit() {
         navigationPosition: 'right',
         navigationTooltips: ['Úvod', 'O mě', 'Služby', 'Portfolio', 'Text', 'Kontakt', 'Odkud jsem'],
         css3: true,
-        responsiveWidth: 1280,
-        responsiveHeight: 700,
+        responsiveWidth: 1200,
+        responsiveHeight: 600,
 
         onLeave: function (index, nextIndex, direction) {
             if (index == 1) {
